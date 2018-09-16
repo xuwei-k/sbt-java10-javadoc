5b31c23eb26feb3742e9c33cd3d656e481146e50
xuwei-k <6b656e6a69@gmail.com>
2018-09-16 14:17:56 +0900

    use javax.tools.ToolProvider.getSystemDocumentationTool

--- Main.scala
+++ Main.scala
@@ -2,19 +2,9 @@ import java.io.PrintWriter
 
 object Test {
 
-  lazy val javadocMethod = {
-    val javadocClass = "com.sun.tools.javadoc.Main"
-    val javadocClz = Class.forName(javadocClass)
-    val (str, pw) = (classOf[String], classOf[PrintWriter])
-    val arrStr = classOf[Array[String]]
-    javadocClz.getDeclaredMethod("execute", str, pw, pw, pw, str, arrStr)
-  }
-
   def execute(args: String*): Int = {
     val stdClass = "com.sun.tools.doclets.standard.Standard"
-    val out = new java.io.PrintWriter(System.out)
-    val run = javadocMethod.invoke(null, "javadoc", out, out, out, stdClass, args.toArray)
-    run.asInstanceOf[java.lang.Integer].intValue
+    javax.tools.ToolProvider.getSystemDocumentationTool.run(System.in, System.out, System.err, args: _*)
   }
 
   def main(args: Array[String]): Unit = {
